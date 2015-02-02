f8b555cf16166524b440523eb6b52d5bf7c9af1c
reddy_quan <reddy_quan@sina.com>
2015-02-02 14:42:09 +0800

    automatization

--- playback.sh
+++ playback.sh
@@ -18,12 +18,12 @@
 #      REVISION:  ---
 #===============================================================================
 #Need busybox to provide awk/sed...
-BUSYBOX=
+BUSYBOX=/data/busybox.miui
 #file=${1:-"record.txt"}    # origin
 file=${1:-"m.txt"}
 
 get_touch_device(){
-    getevent -pl|$BUSYBOX sed -e ':a;N;$!ba;s/\n / /g'|\
+    getevent -pl 2>/dev/null|$BUSYBOX sed -e ':a;N;$!ba;s/\n / /g'|\
         $BUSYBOX grep 'ABS_MT_TOUCH'|$BUSYBOX awk '{print $4}'|$BUSYBOX tr -d '\011\012\015'
 }
 
@@ -32,19 +32,18 @@ touchdev=$(get_touch_device)
 #delete [ ' ' ], and replace '.' with ' '
 $BUSYBOX sed 's/\[//g;s/ *//;s/\]//g;s/\./ /' $file >$file.format
 
-#$BUSYBOX awk '{print $1}' $file.format | $BUSYBOX awk -F. '{print $1, $2}'
-
-cat $file.format | $BUSYBOX awk 'BEGIN{last_t=0}
+cat $file.format | $BUSYBOX awk -v dev=$touchdev 'BEGIN{last_t=0}
 NR==1{OFMT="%.6f";
 last_t=$1"."$2;
 print "#!/system/bin/sh";
-cmd="let num=0x"$3";let num2=0x"$4";let num3=0x"$5";echo sendevent /dev/input/event2 $num $num2 $num3";
+cmd="let num=0x"$3";let num2=0x"$4";let num3=0x"$5";echo sendevent "dev" $num $num2 $num3";
 system(cmd);
 }
 NR>1{OFMT="%.6f";now_t=$1"."$2;
-print "sleep",now_t-last_t;last_t=now_t;
-cmd="let num=0x"$3";let num2=0x"$4";let num3=0x"$5";echo sendevent /dev/input/event2 $num $num2 $num3";
+delay=now_t-last_t;
+if(delay>0)
+print "sleep",delay;
+last_t=now_t;
+cmd="let num=0x"$3";let num2=0x"$4";let num3=0x"$5";echo sendevent "dev" $num $num2 $num3";
 system(cmd);
 }'
-
-
--- record.sh
+++ record.sh
@@ -4,7 +4,7 @@
 #         USAGE: ./record.sh 
 # 
 #   DESCRIPTION: 录制 Android 屏幕事件，保存在 $file 文件上
-# 
+#
 #       OPTIONS: ---
 #  REQUIREMENTS: ---
 #          BUGS: ---
@@ -18,7 +18,7 @@ BUSYBOX=/data/busybox
 file=${1:-"m.txt"}
 
 get_touch_device(){
-    getevent -pl|$BUSYBOX sed -e ':a;N;$!ba;s/\n / /g'|\
+    getevent -pl 2>/dev/null|$BUSYBOX sed -e ':a;N;$!ba;s/\n / /g'|\
         $BUSYBOX grep 'ABS_MT_TOUCH'|$BUSYBOX awk '{print $4}'|$BUSYBOX tr -d '\011\012\015'
 }
 
@@ -26,6 +26,12 @@ touchdev=$(get_touch_device)
 echo "touch device is [ $touchdev ]"
 
 echo "--recorder start now, output file is [ $file ]"
+echo "pid is: $$"
+
 trap "echo  '= recoder had stop ='" SIGINT
-getevent -t $touchdev >$file
+# 用重定向到文件的方法有缺陷，时候半天不出来
+# 执行getevent -t将执行结果保存起来最安全
+#getevent -t $touchdev >$file
+getevent -t $touchdev
+
 echo "--recorder start end, output file is [ $file ]"
